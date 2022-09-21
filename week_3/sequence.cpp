0b616c8c81af3f9e42b186c6817fc042325a99b1
samyhaff <haffoudhisamy@gmail.com>
2022-09-21 17:06:47 +0200

    print solution

--- week_3/sequence.cpp
+++ week_3/sequence.cpp
@@ -6,7 +6,8 @@ using namespace std;
 
 int main() {
     string x, y;
-    cin >> x >> y;
+    getline(cin, x);
+    getline(cin, y);
 
     int n = x.length();
     int m = y.length();
@@ -25,13 +26,52 @@ int main() {
         }
     }
 
-    for (int i = 0; i <= n; i++) {
-        for (int j = 0; j <= m; j++) {
-            cout << arr[i][j] << " ";
-        }
-        cout << endl;
-    }
+    // for (int i = 0; i <= n; i++) {
+    //     for (int j = 0; j <= m; j++) {
+    //         cout << arr[i][j] << " ";
+    //     }
+    //     cout << endl;
+    // }
     // cout << arr[n][m] << endl;
+
+    string x_sol, y_sol;
+    int i = n, j = m;
+
+    while (i > 0 || j > 0) {
+        if (i == 0) {
+            x_sol.push_back('-');
+            y_sol.push_back(j > 0 ? y[j - 1] : '-');
+            j--;
+            continue;
+        }
+        else if (j == 0) {
+            x_sol.push_back(i > 0 ? x[i - 1] : '-');
+            y_sol.push_back('-');
+            i--;
+            continue;
+        }
+        int cost = min({arr[i - 1][j - 1], arr[i][j - 1], arr[i - 1][j]});
+        if (arr[i - 1][j - 1] == cost) {
+            x_sol.push_back(i > 0 ? x[i - 1] : '-');
+            y_sol.push_back(j > 0 ? y[j - 1] : '-');
+            i--; j--;
+        }
+        else if (arr[i][j - 1] == cost) {
+            x_sol.push_back('-');
+            y_sol.push_back(j > 0 ? y[j - 1] : '-');
+            j--;
+        }
+        else if (arr[i - 1][j] == cost) {
+            x_sol.push_back(i > 0 ? x[i - 1] : '-');
+            y_sol.push_back('-');
+            i--;
+        }
+    }
+
+    reverse(x_sol.begin(), x_sol.end());
+    reverse(y_sol.begin(), y_sol.end());
+
+    cout << x_sol << '\n' << y_sol;
 
     return 0;
 }
